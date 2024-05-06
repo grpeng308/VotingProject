586f76bd525d61e1a81b5f8245f632c97637b000
grpeng308 <167130138+grpeng308@users.noreply.github.com>
2024-05-06 14:59:20 -0500

    3
    documentation, typehinting

--- logic.py
+++ logic.py
@@ -7,21 +7,35 @@ from PyQt6.QtWidgets import *
 
 CANDIDATE_ONE = "Jane"
 CANDIDATE_TWO = "John"
-def is_int(str):
+
+
+def is_int(string: str) -> bool:
+    """
+    Method to check if string is an integer
+    :param string: String to be checked
+    :return: True if integer, False otherwise
+    """
     try:
-        int(str)
+        int(string)
         return True
     except ValueError:
         return False
 
 
-
 class Logic(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
+        """
+        Create candidate variable to keep track of who a user votes for
+        Create a list to store votes
+        """
         self.__candidate = "None"
         self.__votes = []
+
+        """
+        Adjust fonts, colors, geometries of labels and buttons
+        """
 
         self.error.setFont(QFont("Helvetica Neue", 11))
         self.error.setGeometry(20, 220, 161, 71)
@@ -39,23 +53,30 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         self.title.setStyleSheet("color: blue")
 
+        """
+        Connect buttons to methods
+        """
+
         self.submit.clicked.connect(lambda: self.submit_clicked())
         self.candidate1.clicked.connect(lambda: self.candidates_clicked())
         self.candidate2.clicked.connect(lambda: self.candidates_clicked())
 
-    def submit_clicked(self):
-        input = self.input_id.text()
+    def submit_clicked(self) -> None:
+        """
+        Method to check ID numbers and perform exception handling
+        """
+        user_input = self.input_id.text()
 
         if self.candidate1.isChecked() or self.candidate2.isChecked():
-            if is_int(input):
-                if len(input) == 8:
-                    if any(input in x for x in self.__votes):
+            if is_int(user_input):
+                if len(user_input) == 8:
+                    if any(user_input in x for x in self.__votes):
                         self.error.setText("Error: ID already voted")
                         self.error.show()
                     else:
                         self.error.setText("Success!")
                         self.error.show()
-                        self.__votes.append(f"{input},{self.__candidate}")
+                        self.__votes.append(f"{user_input},{self.__candidate}")
                         self.input_id.setText("")
                         self.buttonGroup.setExclusive(False)
                         self.candidate1.setAutoExclusive(False)
@@ -81,14 +102,11 @@ class Logic(QMainWindow, Ui_MainWindow):
             for vote in self.__votes:
                 file.write(f"{vote}\n")
 
-    def candidates_clicked(self):
+    def candidates_clicked(self) -> None:
+        """
+        Method to store candidate picked in candidate variable
+        """
         if self.candidate1.isChecked():
             self.__candidate = CANDIDATE_ONE
         else:
             self.__candidate = CANDIDATE_TWO
-
-
-
-
-
-
